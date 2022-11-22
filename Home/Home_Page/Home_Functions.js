52fbcce6f0b053b7e3bc112f4d6a2c0b345b8c3c
codinglabwebsite <115354673+codinglabwebsite@users.noreply.github.com>
2022-11-22 16:51:42 +0100

    Update Home_Functions.js

--- Home/Home_Page/Home_Functions.js
+++ Home/Home_Page/Home_Functions.js
@@ -112,10 +112,6 @@ function myFunction(x)
     //Gyroscope Backgorund
     const handleMotion = e => 
     {
-      const x = Math.round(e.accelerationIncludingGravity.x) * move;
-      const y = Math.round(e.accelerationIncludingGravity.y) * move;
-      const z = Math.round(e.accelerationIncludingGravity.z) * move;
-      
       background.style.transform = `translate3d(0px, 0px, 0px)`;
     }
 
